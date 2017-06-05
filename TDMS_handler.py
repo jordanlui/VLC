ef087bf08082cc210164a09da6d3d86a5fda3b4c
Jordan Lui <jordan.lui@gmail.com>
2017-06-05 14:14:32 +0200

    file importer outputs proper coord into x matrix

--- TDMS_handler.py
+++ TDMS_handler.py
@@ -99,33 +99,40 @@ for file in filelist:
     if datalabelsearch:
         # Set x and y coordinate values. Note these are integers for now
         # Done on an optical breadboard with 1 inch increments
+        # Extract x and y coord
         x = datalabelsearch.group(1)
         x = int(x)
         y = datalabelsearch.group(2)
         y = int(y)
-        # Use function to return mean values and the entire data sets
+        
+        # Use function to return mean values and the entire data sets in extracted columns
         [mean1,mean2,mean3,mean4,c1,c2,c3,c4] = tdmsfuncapr14(file)
+        
         # Format classname in a format we desire        
         classname = 'x'+str(x)+'y'+str(y)
         # Try making a new row that we can work with
-        labelsrow = [classname,x,y]
-        # Store this to a label file
+        labelsrow = [classname,x,y] # This is the string coordinate and numeric x,y coordinates
+        
+        # Append the coordinates into a table
         labels.append(labelsrow) # This is the name table of all possible labels and coordinates
+        
         # Generate column label for data so we can generate matrix
-        labelcolumn = label * np.ones((len(c1),1))
-#        labelcolumn2 = labelsrow * np.ones((len(c1),1))
-#        labeltest = np.asarray(labelsrow)
-#        print np.repeat(labelsrow,3)
+        label = np.reshape(labelsrow[1:],([1,2])) # The actual x,y coordinate values in array
+        labelcolumn = label * np.ones((len(c1),1)) # Make into a matching array size to mate with columns
+
         # Write mean values to data array
         data.append([filename, x,y, mean1,mean2,mean3,mean4])
+        
         # Concatenate our data
         newdata = np.concatenate((labelcolumn,c1,c2,c3,c4),axis=1)
-        # Temporarily cut our new data for faster analysis and loading
-        newdata = newdata[100:700,:]
+ 
+        # If desired, we can cut out some data in each loop to decrease x-matrix size
+#        newdata = newdata[100:700,:]
         
+        # Append values into data2, which contains all data points
         data2.append(newdata)
         
-        label = label+1 # increment our archaic label ordinal system. soon to be removed.
+#        label = label+1 # increment our archaic label ordinal system. soon to be removed.
 #        exportresults(os.path.join(pathout,"x.csv"),newdata)
 #        print dataoutput
         
